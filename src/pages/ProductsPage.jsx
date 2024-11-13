eca92d94f30e204c3afbf27dde61c9cbc161e56d
Davoud A.Khameneh <mt.d.abdollahian@gmail.com>
2024-11-13 11:12:01 +0330

    input for search

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
@@ -8,19 +11,38 @@ export default function ProductsPage() {
 
     const products = useProducts();
 
+    const [ search, setSearch ] = useState("");
+
+    const searchHandler = () => {
+
+        console.log("search");
+
+    }
+
     return (
 
-        <div className={ styles.container } >
+        <>
 
-            <div className={ styles.products } > 
+            <div>
 
-                { !products.length && <Loader /> }
-                { products.map( p => <Card key={ p.id }  data={ p }  /> ) } 
-                
+                <input type="text" placeholder="Search ..." value={ search } onChange={ e => setSearch( e.target.value.toLowerCase().trim() ) } />
+                <button onClick={ searchHandler } > <ImSearch /> </button>
+
             </div>
-            <div>sidebar</div>
 
-        </div>
+            <div className={ styles.container } >
+
+                <div className={ styles.products } > 
+
+                    { !products.length && <Loader /> }
+                    { products.map( p => <Card key={ p.id }  data={ p }  /> ) } 
+                
+                </div>
+                <div>sidebar</div>
+
+            </div>
+
+        </>
 
     )
 
